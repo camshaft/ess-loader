69f0b41b1ec8f604aa74b22270a8f0be95d9c9f3
camshaft <bytheway.cameron@gmail.com>
2015-01-20 16:13:07 -0700

    more fixes

--- index.js
+++ index.js
@@ -4,35 +4,72 @@
 
 var renderCSS = require('./to-css').renderCSS;
 
-module.exports = function(source) {
+exports = module.exports = function(source) {
   this.cacheable && this.cacheable();
   var q = this.resourceQuery || '';
-  if (~q.indexOf('dynamic')) return loadDynamic.call(this, source);
-  if (~q.indexOf('raw')) return loadRaw.call(this, source);
-  return loadStatic.call(this, source);
+  if (~q.indexOf('dynamic')) return source + compileDynamic();
+  if (~q.indexOf('raw')) return source + compileRaw();
+  return compileStatic.call(this, source);
 };
 
-function loadDynamic(source) {
-  return source + '\n' +
-    'import {toCSS} from ' + resolve('./to-css') + '\n' +
-    'export default require("onus-style")(exports, toCSS);';
+exports.pitch = function(remainingRequest) {
+  this.cacheable && this.cacheable();
+  var req = remainingRequest.split('!');
+  var q = this.resourceQuery || '';
+  if (~q.indexOf('dynamic')) return pitchDynamic.call(this, req);
+  if (~q.indexOf('raw')) return pitchRaw.call(this, req);
+};
+
+function pitchDynamic(req) {
+  return '' +
+    'exports = module.exports = require(' + JSON.stringify('!!' + req.join('!')) + ');\n' +
+    compileDynamic();
 }
 
-function loadRaw(source) {
-  return source + '\n' +
-    'import {renderCSS} from ' + resolve('./to-css') + '\n' +
-    'export default renderCSS(exports);\n';
+function pitchRaw(req) {
+  return '' +
+    'exports = module.exports = require(' + JSON.stringify('!!' + req.join('!')) + ');\n' +
+    compileRaw();
 }
 
-function loadStatic(source) {
-  var req = 'require = require(' + resolve('enhanced-require') + ')(module, ' + JSON.stringify({
+function compileDynamic() {
+  return '\n' +
+    'var toCSS = require(' + resolve('./to-css') + ')["toCSS"];\n' +
+    'exports["default"] = require("onus-style")(exports, toCSS);';
+}
+
+function compileRaw() {
+  return '\n' +
+    'var renderCSS = require(' + resolve('./to-css') + ')["renderCSS"];\n' +
+    'exports["default"] = renderCSS(exports);';
+}
+
+function compileStatic(source) {
+  var opts = this.options;
+
+  var loaders = opts.module ? opts.module.loaders : opts.resolve.loaders;
+
+  var er = 'require = require(' + resolve('enhanced-require') + ')(module, require(' + resolve('./json2regexp') + ')(' + JSON.stringify({
     recursive: true,
-    resolve: this.options.resolve
-  }) + ');\n'
-  var mod = this.exec(req + source, this.resource);
-  return renderCSS(mod)();
+    resolve: {
+      loaders: loaders,
+      extensions: opts.resolve.extensions,
+      modulesDirectories: opts.resolve.modulesDirectories
+    }
+  }, toString) + '));\n'
+
+  var mod = this.exec(er + source, this.resource);
+
+  var out = renderCSS(mod)();
+
+  return out;
 }
 
 function resolve(path) {
   return JSON.stringify(require.resolve(path));
+}
+
+function toString(key, value) {
+  if (!(value instanceof RegExp)) return value;
+  return value.toString();
 }
