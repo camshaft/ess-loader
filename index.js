fea0d3a8e9a6a59a460dd1489e6c2796affaac42
camshaft <bytheway.cameron@gmail.com>
2016-01-16 17:09:55 -0700

    add postcss plugin support

--- index.js
+++ index.js
@@ -2,14 +2,17 @@
  * Module dependencies
  */
 
+var utils = require('loader-utils');
+
 exports = module.exports = function(source) {};
 exports.pitch = function(remainingRequest) {
   this.cacheable && this.cacheable();
   var req = JSON.stringify('!!' + remainingRequest);
   var q = this.resourceQuery || '';
-  if (~q.indexOf('dynamic')) return pitchDynamic.call(this, req);
-  if (~q.indexOf('raw')) return pitchRaw.call(this, req);
-  return pitchStatic.call(this, req);
+  var opts = utils.parseQuery(this.query);
+  if (~q.indexOf('dynamic')) return pitchDynamic.call(this, req, opts);
+  if (~q.indexOf('raw')) return pitchRaw.call(this, req, opts);
+  return pitchStatic.call(this, req, opts);
 };
 
 function pitchDynamic(req) {
@@ -24,10 +27,23 @@ function pitchRaw(req) {
     'exports["default"] = require(' + resolve('ess-compiler/render') + ')(exports);';
 }
 
-function pitchStatic(req) {
-  return '' +
-    'var content = require(' + req + ');\n' +
-    'module.exports = [[module.id, require(' + resolve('ess-compiler/render') + ')(content)(), ""]];';
+function pitchStatic(req, opts) {
+  return [
+    'var content = require(' + req + ');',
+    'var css = require(' + resolve('ess-compiler/render') + ')(content)();',
+    'module.exports = [[module.id, ' + process('css', opts) + ', ""]];'
+  ].join('\n');
+}
+
+function process(input, opts) {
+  var plugins = opts.postcss && (Array.isArray(opts.postcss) ? opts.postcss : opts.postcss.split(','));
+  if (!plugins || !plugins.length) return input;
+
+  var plugins = plugins.map(function(p) {
+    return 'require(' + resolve(p) + ')';
+  }).join(', ');
+
+  return '"" + require(' + resolve('postcss') + ')([' + plugins + ']).process(' + input + ').css';
 }
 
 function resolve(path) {
